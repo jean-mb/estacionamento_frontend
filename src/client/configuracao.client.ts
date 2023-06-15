d2e18022d4c03f0e3cc56200b5d3f9d62473965e
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-06-15 20:54:58 -0300

    client de configuracao concluido

---
+++ src/client/configuracao.client.ts
@@ -0,0 +1,43 @@
+import { Configuracao } from '@/model/configuracao'
+import axios, { AxiosInstance } from 'axios'
+
+export class CondutorClient {
+  private axiosClient: AxiosInstance
+
+  constructor() {
+    this.axiosClient = axios.create({
+      baseURL: 'http://localhost:8080/api/condutor',
+      headers: { 'Content-type': 'application/json' }
+    })
+  }
+
+  public async getConfiguracao(): Promise<Configuracao> {
+    try {
+      const response = await this.axiosClient.get<Configuracao>('')
+      return response.data
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+
+  public async primeiraConfiguracao(configuracao: Configuracao): Promise<Configuracao> {
+    try {
+      const response = await this.axiosClient.post<Configuracao>('', configuracao)
+      return response.data
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+
+  public async editar(configuracao: Configuracao): Promise<Configuracao> {
+    try {
+      const response = await this.axiosClient.put<Configuracao>(
+        '/editar',
+        configuracao
+      )
+      return response.data
+    } catch (error) {
+      return Promise.reject(error)
+    }
+  }
+}
