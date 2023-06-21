ded09f5d9d622531752a0d08729f01723f874b89
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-06-20 23:31:04 -0300

    listar de marcas ok

--- src/views/MarcasView.vue
+++ src/views/MarcasView.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container" style="margin-top: 10px">
-    <div class="row">
+  <div class="container">
+    <div class="row mt-5">
       <div class="col-md-10 text-start">
         <p class="fs-3">Marcas</p>
       </div>
-      <div class="col-md-2">
+      <div class="col-md-2 col-md-2 align-self-center">
         <div class="d-grid gap-2">
           <router-link
             type="button"
@@ -15,63 +15,56 @@
         </div>
       </div>
     </div>
-
-    <div class="row">
-      <div class="col-md-12">
-        <table class="table">
-          <thead class="table-secondary">
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Ativo</th>
-              <th scope="col" class="text-start">Marca</th>
-              <th scope="col">Opção</th>
-            </tr>
-          </thead>
-          <tbody class="table-group-divider">
-            <tr v-for="item in marcasList" :key="item.id">
-              <th class="col-md-1">{{ item.id }}</th>
-              <th class="col-md-2">
-                <span v-if="item.ativo" class="badge text-bg-success">
-                  Ativo
-                </span>
-                <span v-if="!item.ativo" class="badge text-bg-danger">
-                  Inativo
-                </span>
-              </th>
-              <th class="text-start">{{ item.nome }}</th>
-              <th class="col-md-2">
-                <div
-                  class="btn-group"
-                  role="group"
-                  aria-label="Basic mixed styles example"
-                >
-                  <router-link
-                    type="button"
-                    class="btn btn-sm btn-warning"
-                    :to="{
-                      name: 'marca.form.editar',
-                      query: { id: item.id, form: 'editar' }
-                    }"
-                  >
-                    Editar
-                  </router-link>
-                  <router-link
-                    type="button"
-                    class="btn btn-sm btn-danger"
-                    :to="{
-                      name: 'marca.form.desativar',
-                      query: { id: item.id, form: 'desativar' }
-                    }"
-                  >
-                    Excluir
-                  </router-link>
-                </div>
-              </th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <table class="table">
+      <thead class="table-dark">
+        <tr>
+          <th class="text-center">ID</th>
+          <th>Nome</th>
+          <th>Status</th>
+          <th class="text-center">Opções</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in marcasList" :key="item.id">
+          <th class="text-center col-md-1">{{ item.id }}</th>
+          <th class="col-md-2">{{ item.nome }}</th>
+          <th class="text-start col-md-2">
+            <span v-if="item.ativo" class="badge text-bg-success"> Ativo </span>
+            <span v-if="!item.ativo" class="badge text-bg-danger">
+              Inativo
+            </span>
+          </th>
+          <th class="text-center col-md-2">
+            <div
+              class="btn-group"
+              role="group"
+              aria-label="Basic mixed styles example"
+            >
+              <router-link
+                type="button"
+                class="btn btn-sm btn-warning"
+                :to="{
+                  name: 'marca.form.editar',
+                  query: { id: item.id, form: 'editar' }
+                }"
+              >
+                Editar
+              </router-link>
+              <router-link
+                type="button"
+                class="btn btn-sm btn-danger"
+                :to="{
+                  name: 'marca.form.desativar',
+                  query: { id: item.id, form: 'desativar' }
+                }"
+              >
+                Excluir
+              </router-link>
+            </div>
+          </th>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -93,8 +86,9 @@ export default defineComponent({
   },
   methods: {
     findAll() {
-      const marcaClient =  new MarcaClient()
-      marcaClient.listarAll()
+      const marcaClient = new MarcaClient()
+      marcaClient
+        .listarAll()
         .then(sucess => {
           this.marcasList = sucess
         })
@@ -106,6 +100,12 @@ export default defineComponent({
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import 'node_modules/bootstrap/scss/bootstrap.scss';
+.container {
+  width: 60%;
+}
+.btn {
+  font-weight: var(--font-weight-forte);
+}
 </style>
