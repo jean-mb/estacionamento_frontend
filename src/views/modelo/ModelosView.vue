368e97bd39a8b79b6e3daf381bcda098b44c4d61
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-06-25 01:00:11 -0300

    ModelosView

--- src/views/modelo/ModelosView.vue
+++ src/views/modelo/ModelosView.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <div class="row mt-5">
       <div class="col-md-10 text-start">
-        <p class="fs-5 m-0">Marcas</p>
+        <p class="fs-5">Modelos</p>
       </div>
-      <div class="col-md-2 col-md-2 align-self-center mb-2">
+      <div class="col-md-2 col-md-2 align-self-center">
         <div class="d-grid gap-2">
           <router-link
             type="button"
@@ -20,27 +20,49 @@
         <tr>
           <th class="text-center">ID</th>
           <th>Nome</th>
+          <th>Marca</th>
           <th>Status</th>
           <th class="text-center">Opções</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in marcasList" :key="item.id">
-          <th class="text-center align-middle col-md-1">{{ item.id }}</th>
-          <th class="col-md-2 align-middle">{{ item.nome }}</th>
-          <th class="text-start align-middle col-md-2">
+        <tr v-for="item in modelosList" :key="item.id">
+          <th class="align-middle text-center col-md-1">{{ item.id }}</th>
+          <th class="align-middle col-md-3">{{ item.nome }}</th>
+          <th class="align-middle text-start col-md-3">
             <span v-if="item.ativo" class="badge text-bg-success"> Ativo </span>
             <span v-if="!item.ativo" class="badge text-bg-danger">
               Inativo
             </span>
           </th>
-
-          <th class="align-middle text-center col-md-2">
-            <BotoesAcoes
-              editarRoute="marca.form.editar"
-              desativarRoute="marca.form.desativar"
-              :id="item.id"
-            ></BotoesAcoes>
+          <th class="align-middletext-start col-md-3">{{ item.marca.nome }}</th>
+          <th class="align-middletext-center col-md-2">
+            <div
+              class="btn-group"
+              role="group"
+              aria-label="Basic mixed styles example"
+            >
+              <router-link
+                type="button"
+                class="btn btn-sm btn-warning"
+                :to="{
+                  name: 'marca.form.editar',
+                  query: { id: item.id, form: 'editar' }
+                }"
+              >
+                Editar
+              </router-link>
+              <router-link
+                type="button"
+                class="btn btn-sm btn-danger"
+                :to="{
+                  name: 'marca.form.desativar',
+                  query: { id: item.id, form: 'desativar' }
+                }"
+              >
+                Excluir
+              </router-link>
+            </div>
           </th>
         </tr>
       </tbody>
@@ -50,30 +72,27 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import BotoesAcoes from '@/components/BotoesAcoes.vue'
-import { MarcaClient } from '@/client/marca.client'
-import { Marca } from '@/model/marca'
+
+import { ModeloClient } from '@/client/modelo.client'
+import { Modelo } from '@/model/modelo'
 
 export default defineComponent({
-  name: 'MarcasView',
+  name: 'ModelosView',
   data() {
     return {
-      marcasList: new Array<Marca>()
+      modelosList: new Array<Modelo>()
     }
-  },
-  components: {
-    BotoesAcoes
   },
   mounted() {
     this.findAll()
   },
   methods: {
     findAll() {
-      const marcaClient = new MarcaClient()
-      marcaClient
-        .listarAll()
+      const modeloClient = new ModeloClient()
+      modeloClient
+        .listaCompleta()
         .then(sucess => {
-          this.marcasList = sucess
+          this.modelosList = sucess
         })
         .catch(error => {
           console.log(error)
@@ -84,8 +103,17 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+$theme-colors: (
+  'dark': #111111,
+  'primary': #515151,
+  'secondary': #c8c8c8,
+  'info': #a4a4a4,
+  'success': #198754,
+  'warning': #ffc107,
+  'danger': #dc3545
+);
 @import 'node_modules/bootstrap/scss/bootstrap.scss';
 .container {
-  width: 60%;
+  width: 70%;
 }
 </style>
