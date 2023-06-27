142c7bf1c62bd27d08ee2297347df7422d401c47
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-06-27 20:48:55 -0300

    tabela de vagas disponiveis

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container mt-5">
+  <div class="d-flex flex-column container mt-5">
     <div class="d-flex justify-content-center">
       <router-link type="button" class="btn btn-primary w-25 fs-5" to="/movimentacao/formulario">Registrar Entrada
       </router-link>
     </div>
-    <table class="table">
-      <thead class="table-dark">
+    <table class="mt-5 table w-25 align-self-center">
+      <thead class="table-secondary">
         <tr>
           <th class="text-center col-md-1">Carro</th>
           <th class="text-center col-md-2">Moto</th>
@@ -13,14 +13,14 @@
         </tr>
       </thead>
       <tbody>
-        <th>
-        <th class="text-center col-md-1">{{ carro }}</th>
-        <th class="text-center col-md-2">{{ moto }}</th>
-        <th class="text-center col-md-2">{{ van }}</th>
-        </th>
+        <tr>
+          <td class="text-center align-middle col-md-1">{{ carro }}</td>
+          <td class="text-center align-middle col-md-2">{{ moto }}</td>
+          <td class="text-center align-middle col-md-2">{{ van }}</td>
+        </tr>
       </tbody>
     </table>
-    <div class="row mt-5">
+    <div class="row mt-3">
       <div class="col-md-10 text-start">
         <p class="fs-5">Movimentações Abertas</p>
       </div>
@@ -29,8 +29,8 @@
       <thead class="table-dark">
         <tr>
           <th class="text-center col-md-1">ID</th>
-          <th class="text-center col-md-2">Placa</th>
           <th class="text-center col-md-2">Vaga</th>
+          <th class="text-center col-md-2">Placa</th>
           <th class="text-center col-md-3">Condutor</th>
           <th class="text-center col-md-2">Data Entrada</th>
           <th class="text-center col-md-3">Opções</th>
@@ -90,9 +90,6 @@ export default defineComponent({
   },
   methods: {
     getVagas() {
-      var qtdCarro: number
-      var qtdMoto: number
-      var qtdVan: number
       const configClient = new ConfiguracaoClient()
       const movsClient = new MovimentacaoClient()
       configClient.getConfiguracao().then(sucess => {
@@ -140,8 +137,8 @@ export default defineComponent({
 <style lang="scss">
 $theme-colors: (
   'dark': #111111,
-  'primary': #515151,
-  'secondary': #c8c8c8,
+  'primary': #2b2727,
+  'secondary': #5c5757,
   'info': #a4a4a4,
   'success': #198754,
   'warning': #ffc107,
